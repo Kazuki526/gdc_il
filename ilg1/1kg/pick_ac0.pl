12b25879fb90d9117276619b899b32fa582b95f8
Kazuki:ilg1 <takaha5.26@gmail.com>
2017-03-24 14:41:06 +0900

    pick AC=0 comp

--- ilg1/1kg/pick_ac0.pl
+++ ilg1/1kg/pick_ac0.pl
@@ -14,41 +14,77 @@ my $hg38=$ENV{"HOME"}."/liftover/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
 
 my $bed_file="/Volumes/areca42TB/tcga/maf_norm/top_driver105.bed";
 (-e $bed_file) or die "ERROR:topdriver bed file:$bed_file is moved?\n";
-
+print "reading bed file\n";
+my %bed=();
+open(BED,"$bed_file");
+while(<BED>){
+		chomp;
+		my @line=split(/\t/,);
+		$line[0] =~ s/^chr//;
+		for(my $i=$line[1];$line[2] >= $i;$i++){
+				$bed{"$line[0]:$i"}="ok";
+		}
+}
+close BED;
 
 my @files=`ls $vcfpath|grep 'vcf.gz\$'`;chomp @files;
-
-open(OUT,">/working/1000genomes/maf/extarct/ac0_variation.tsv");
-print OUT "chr\tstart\tref\talt\tac_1kg\tan_1kg\n";
+my @lines=();
 foreach my $file (@files){
-		my @lines=`gunzip -c $vcfpath/$file|grep AC=0`;
+		push(@lines,`gunzip -c $vcfpath/$file|grep AC=0`);
 		push(@lines,`gunzip -c $vcfpath/$file|grep 'AC=[0-9]\+,0'`);
 		push(@lines,`gunzip -c $vcfpath/$file|grep 'AC=[0-9]\+,[0-9]\+,0;'`);
-		foreach my $line(@lines){
-				chomp $line;
-				my @line=split(/\t/,$line);
-				my ($posi,$ref,$alt,$an);
-				if($line[4]=~/,/){
-						my @alt=split(/,/$line[4]);
-						my @ac;
-						if($line[7] =~/AC=([0-9,]+);/){
-								@ac=split(/,/,$1);
-								for(my $i=0;@alt>$i;$i++){
-										if($ac[$i]==0){$alt=$alt[$i];}
-								}
-								if(length($line[3]) !=length($alt)){next;}
-								elsif((length($line[3]) > 1)||(length($alt) > 1)){
-										($posi,$ref,$alt)=&indel2snp($line[1],$line[3],$alt);
-								}else{($posi,$ref)=($line[1],$line[3]);}
-						}
-				}else{
+}
+my $bed="/working/1000genomes/maf/extract/ac0.bed";
+open(OUT,">$bed");
+my %ac0=();
+foreach my $line(@lines){
+		chomp $line;
+		my @line=split(/\t/,$line);
+		my ($posi,$ref,$alt,$an);
+		if($line[4]=~/,/){
+				my @alt=split(/,/,$line[4]);
+				my @ac;
+				if($line[7] =~/AC=([0-9,]+);.+;AN=(\d+);/){
+						@ac=split(/,/,$1);
+						$an=$2;
+				}else{die "ERROR:AC nodata at $line\n";}
+				for(my $i=0;@alt>$i;$i++){
+						if($ac[$i]==0){$alt=$alt[$i];}
+				}
+				if(length($line[3]) !=length($alt)){next;}
+				elsif((length($line[3]) > 1)||(length($alt) > 1)){
+						($posi,$ref,$alt)=&indel2snp($line[1],$line[3],$alt);
+				}else{($posi,$ref)=($line[1],$line[3]);}
+		}else{
 				if($line[7]=~/AN=(\d+);/){
 						$an=$1;
 				}
-				print OUT "$line[0]\t$line[1]\t$line[3]\t$alt\t0\t$an\n";
+				($posi,$ref,$alt)=($line[1],$line[3],$line[4]);
 		}
+		my $end=$posi+1;
+		print OUT "chr$line[0]\t$posi\t$end\t$line[0]:$posi\n";
+		$ac0{"$line[0]:$posi"}="$ref\t$alt\t0\t$an";
 }
 close OUT ;
+
+my %remap= map{chomp;my @c=split(/\t/,);$c[0]=~s/^chr//;($c[3]=>"$c[0]:$c[1]")}`$liftover $bed $chainfile /dev/stdout /dev/null 2>/dev/null`;
+open(OUT,">/working/1000genomes/maf/extract/ac0_variation.tsv");
+print OUT "chr\tstart\tref\talt\tac_1kg\tan_1kg\n";
+foreach my $region(sort(keys %ac0)){
+		if(defined $bed{$remap{$region}}){
+				my ($chr,$start)=split(/:/,$remap{$region});
+				my ($ref,$alt,$ac,$an)=split(/\t/,$ac0{$region});
+				my $ref_liftovered=`samtools faidx $hg38 $chr:$start-$start`;
+				if($ref ne $ref_liftovered){
+						if($alt ne $ref_liftovered){$ref=$ref_liftovered;
+						}else{next;}
+				}
+				print OUT "$chr\t$start\t$ref\t$alt\t$ac\t$an\n";
+		}
+}
+close OUT;
+
+
 exit;
 
 
@@ -57,11 +93,11 @@ exit;
 sub indel2snp {
 		my ($start,$ref,$alt)=@_;
 		while($ref and $alt and substr($ref,0,1) eq substr($alt,0,1) and $ref ne $alt){
-				($ref, $alt)=map{$_=substr(( $_, 1 ); ( $_ ? $_ : "-" )} ( $ref, $alt );
+				($ref, $alt)=map{$_=substr( $_, 1 ); ( $_ ? $_ : "-" )} ( $ref, $alt );
 				$start++;
 		}
 		while( $ref and $alt and substr( $ref, -1, 1 ) eq substr( $alt, -1, 1 ) and $ref ne $alt ) {
-				( $ref, $alt ) = map{$_ = substr( $_,-1, 1 ); ( $_ ? $_ : "-" )} ( $ref, $alt );
+				( $ref, $alt ) = map{$_ = substr( $_,0,-1  ); ( $_ ? $_ : "-" )} ( $ref, $alt );
 		}
 		return($start,$ref,$alt);
 }
