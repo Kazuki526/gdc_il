fc9c8383f9773e85e5d191d6e8cf7629a87b1609
Kazuki:ilg1 <takaha5.26@gmail.com>
2017-04-06 20:03:22 +0900

    change to download norm bam

--- ilg1/download/check_reload_bam.pl
+++ ilg1/download/check_reload_bam.pl
@@ -2,11 +2,18 @@
 use warnings;
 use strict;
 
-#dir check
+#dir check #### this script is download norm_bam only
+my $pwd=`pwd`;chomp $pwd;
+if($pwd ne "/Volumes/areca42TB2/gdc/norm_bam"){die "!!ERROR!! doing on wrong dir\n";}
 
-my $token_file=`ls ~/git/gdc_il/|grep '^gdc-user-token'`;chomp $token_file;
-my $token=`cat ~/git/gdc_il/$token_file`;
+my $token_path=`ls ~/git/gdc_il|grep 'gdc-user-token'`;
+if(!$token_path){die "!!ERROR!!:token file not exitst!!";}
+chomp $token_path;
+$token_path="~/git/gdc_il/$token_path";
+my $token=`cat $token_path`;
+
 my $bp=$ARGV[0];
+my $bamdir="$bp";
 my $manifest = `ls $bp|grep '^gdc_manifest'`;chomp $manifest;
 
 use Parallel::ForkManager;
@@ -15,6 +22,8 @@ my $pm = new Parallel::ForkManager(10);
 open(MANI,"$bp/$manifest");
 my $devnull=<MANI>;
 my $linen=0;
+my $json=$ENV{"HOME"}."/git/gdc_il/ilp27/maf_norm/top_driverallexon_json.txt";
+($json and -e $json) or die "ERROR:$json not exist!!";
 while(<MANI>){
 		$linen++;
 		#forks and returns the pid for child
@@ -23,22 +32,27 @@ while(<MANI>){
 		chomp ;
 		my @line=split(/\t/,);
 		print "$linen:$line[1] doing\n";
-		my $taila=`samtools view $bp/$line[1] |tail -n 1`;
-		if(!defined $taila){$taila="0\t0\t0";}
+		my $taila=`samtools view $bamdir/$line[1] |tail -n 1`;
+		if(!$taila){$taila="0\t0\t0\t0";}
 		my @taila=split(/\t/,$taila);
 		if(($taila[2] ne "chrX")||($taila[3] < 134428791 )){
-				print "$linen:$line[1]  $taila[2]:$tailb[3] eq not chrXorY so download again\n";
+				print "$linen:$line[1] chr $taila[2]:$taila[3] eq not chrX:134428791~ so download again\n";
 				my $focal=0;
 				while($focal==0){
-						system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@top_driverallexon_json.txt --output $bp/$line[1] > /dev/null 2>&1");
-						my $tailb=`samtools view $bp/$line[1] |tail -n 1`;
+						system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $bamdir/$line[1] > /dev/null 2>&1");
+						my $tailb=`samtools view $bamdir/$line[1] |tail -n 1`;
+						if(!$tailb){$tailb="0\t0\t0\t0";}
 						my @tailb=split(/\t/,$tailb);
-						if(!defined $tailb){$taila="0\t0\t0";}
-						if(($tailb[2] ne "chrX")&&($tailb[3] > 134428791 )&&($taila ne $tailb)){$taila=$tailb;
-						}else{
+						if(($tailb[2] eq "chrX")&&($tailb[3] > 134428791)){
 								$focal++;
 								print "$linen:$line[1] redownloaded $tailb[2]:$tailb[3] is ok\n";
-								}
+						}elsif(($tailb[3] != 0)&&(`samtools view $bamdir/$line[1] 2>&1|head -n 1` !~ /EOF\smarker\sis\sabsent/)){
+								$focal++;
+								print "$linen:$line[1] redownloaded $tailb[2]:$tailb[3] is ok\n";
+						}else{
+								$taila=$tailb;
+								print "$linen:$line[1] download error. download again\n";
+						}
 				}
 		}
 
