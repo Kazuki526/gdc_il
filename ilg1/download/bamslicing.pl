f479dec2472511536fa0828d68554bf11ebe5996
Kazuki:ilg1 <takaha5.26@gmail.com>
2017-11-28 20:50:24 +0900

    remake bamslicing script

--- ilg1/download/bamslicing.pl
+++ ilg1/download/bamslicing.pl
@@ -2,9 +2,12 @@
 use strict;
 use warnings;
 
-#dir chech
-my $pwd=`pwd`;chomp $pwd;
-if($pwd ne "/Volumes/areca42TB2/gdc/tumor_bam"){die "ERROR: doing on wrong dir !!\n";}
+############   perl bamslicing.pl  MANIFEST BED_JSON OUT_DIR
+
+#input check?
+my ($manifes,$json,$out_dir)=@ARGV;
+if( $manifest !~/manifest/|$json !~/json$/){die "ERROR::input files are correct??\n");
+(-e $out_dir) or die "ERROR::out dir is not exist!\n";
 
 #perl bamslicing.pl MANIFEST.tsv out_DIR maxparallel
 my $token_path=`ls ~/git/gdc_il|grep 'gdc-user-token'`;
@@ -13,24 +16,21 @@ chomp $token_path;
 $token_path="~/git/gdc_il/$token_path";
 my $token=`cat $token_path`;
 
-my $bp=$ARGV[0];
-my $bamdir=$bp;
-my $manifest=`ls $bamdir|grep '^gdc_manifest'`;chomp $manifest;
-($manifest and -e "$bamdir/$manifest") or die "ERROR: there arenot manifest file!!\n";
+($manifest and -e "$manifest") or die "ERROR: there arenot manifest file!!\n";
+($json     and -e "$json"    ) or die "ERROR: there arenot json file!!\n";
 
-open(MAN,"$bamdir/$manifest");
-#my($start_line_num,$end_line_num)=($ARGV[2],$ARGV[3]);
-my$linen=0;
+open(MAN,"$manifest");
+my $linen=0;
 use Parallel::ForkManager;
 my $max_processes=10;
 my $pm = new Parallel::ForkManager($max_processes);
-my $dev_null=<MAN>;
-my $json=$ENV{"HOME"}."/git/driver_genes/onlytop105/top_driver105exon_json.txt";
-(-e $json) or die "ERROR:$json not exist!!\n";
+my $header=<MAN>;
+my @header = split(/\t/,$header);
+if($header[0] != "id"|$header[1] != "filename"){die "ERROR::manifest file colume is correct??\n";}
 
+open (ERR,">$out_dir/result_download.txt");
 while(<MAN>){
 		$linen++;
-		
 		#forks and returns the pid for child
 		my $pid = $pm->start and next;
 
@@ -38,38 +38,35 @@ while(<MAN>){
 #		}elsif($end_line_num < $line_num){last;}
 		chomp;
 		my @line=split(/\t/,);
-		my @ls=`ls $bamdir`;chomp @ls;
+		my @ls=`ls $out_dir`;chomp @ls;
 		if(!grep{$_ eq "$line[1]"}@ls){
 				print "$linen:curl $line[1] now\n";
-				system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $bamdir/$line[1] > /dev/null 2>&1");
+				print ERR "$linen:curl $line[1] now\n";
+				system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $our_dir/$line[1] > /dev/null 2>&1");
 		}
-		print "$linen:$line[1] checking\n";
-		my $taila;
 		my $focal=0;
 		my $time=0;
 		while($focal==0){
 				$time++;
-				my $tailb=`samtools view $bamdir/$line[1] |tail -n 1`;
-				if(!$tailb){$tailb="0\t0\t0\t0";}
-				my @tailb=split(/\t/,$tailb);
 				if($time >10){
-						print "$linen:$line[1] download more than 10 times so this file cannot download? redownload $tailb[2]:$tailb[3]\n";
+						print "$linen:$line[1] download more than 10 times so this file cannot download?\n";
+						print ERR "$linen:$line[1] download more than 10 times so this file cannot download?\n";
 						$focal++;
-				}elsif(($tailb[2] eq "chrX")&&($tailb[3] > 134428789)){
+				}elsif(`samtools view $out_dir/$line[1] 2>&1|head -n 1` !~ /EOF\smarker\sis\sabsent/){
 						$focal++;
-						print "$linen:$line[1] redownloaded $tailb[2]:$tailb[3] is ok\n";
-				}elsif(($tailb[3] != 0)&&(`samtools view $bamdir/$line[1] 2>&1|head -n 1` !~ /EOF\smarker\sis\sabsent/)){
-						$focal++;
-						print "$linen:$line[1] redownloaded $tailb[2]:$tailb[3] is ok\n";
+						print "$linen:$line[1] downloaded is ok\n";
+						print ERR "$linen:$line[1] downloaded is ok\n";
 				}else{
-						$taila=$tailb;
 						print "$linen:$line[1] download error. download again\n";
+						print ERR "$linen:$line[1] download error. download again\n";
 				}
 				if($focal==0){
-						system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $bamdir/$line[1] > /dev/null 2>&1");
+						system("curl --header \"X-Auth-Token: $token\" --request POST https://gdc-api.nci.nih.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $out_dir/$line[1] > /dev/null 2>&1");
 				}
 		}
 
 		$pm->finish; #terminates the child process
 }
+close MAN;
+close ERR;
 exit;
