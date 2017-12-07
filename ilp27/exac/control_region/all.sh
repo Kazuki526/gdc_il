0fc8f9f86f46aec9753d115664b0f97d8b969a91
Kazuki:ilp27 <takaha5.26@gmail.com>
2017-12-07 21:40:40 +0900

    exac extracting program for control region

---
+++ ilp27/exac/control_region/all.sh
@@ -0,0 +1,19 @@
+#doing on focal dir
+export PWD=`pwd`
+
+perl ~/git/gdc_il/ilp27/exac/control_region/pick_region.pl single_nonoverlap.tsv
+
+perl ~/git/gdc_il/ilp27/exac/control_region/lift_over_and_ref_check.pl
+
+
+export PERL5LIB=/usr/local/lib/perl5/site_perl:${PERL5LIB} #vcftools neeed this 
+
+export VEP_PATH=$HOME/vep
+export VEP_DATA=$HOME/.vep
+export PERL5LIB=$VEP_PATH:$PERL5LIB
+export PATH=$VEP_PATH/htslib:$PATH
+export PATH=$HOME/vep/samtools/bin:$PATH
+cd ~/mskcc-vcf2maf-be943f6
+perl vcf2maf_chr.pl --input-vcf $PWD/exac_nontcga_liftovered.vcf --output-maf $PWD/exac_nontcga_control_region.maf --vep-path $VEP_PATH --vep-data $VEP_DATA --ref-fasta ~/.vep/homo_sapiens/86_GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa --ncbi-build GRCh38
+
+perl vcf2maf_chr.pl --input-vcf $PWD/exac_nontcga_liftovered_indel.vcf --output-maf $PWD/exac_nontcga_control_region_indel.maf --vep-path $VEP_PATH --vep-data $VEP_DATA --ref-fasta ~/.vep/homo_sapiens/86_GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa --ncbi-build GRCh38
