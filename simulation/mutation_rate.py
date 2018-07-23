70f5c2da82bbaa3837637a8b5d6f217ee12d5d74
Kazuki:kaz_ilg <takaha5.26@gmail.com>
2018-07-23 22:48:25 +0900

    make simulation dir

---
+++ simulation/mutation_rate.py
@@ -0,0 +1,10 @@
+#! /usr/local/bin/env python
+# -*- coding: utf-8 -*-
+import random
+
+class Individual:
+  def __init__(self, id_num, fitness=1):
+    self._id = id_num
+    self._fitness = _fitness
+
+    @
