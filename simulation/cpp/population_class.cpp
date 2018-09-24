76991d870dc006b205f6e6138f35023f791b426e
kaz_mbp16 <takaha5.26@gmail.com>
2018-09-25 07:26:24 +0900

    on the way

--- simulation/cpp/population_class.cpp
+++ simulation/cpp/population_class.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+count int N = 10;
 
 class Individual
 {
+private:
+  int id;
+public:
+  Individual(int i=1){id = i;}
+  void set_id(int i){id = i;}
+}
+
+class Population : public Individual
+{
+private:
+  Individual individual[N]
+public:
+  Population()
+  void next_generation()
+}
+
+Population::Population(){
+  for(int id=1; 10 >= id;id++){
+    individual[i] = Individual.set_id(i);
+  }
+}
+
+void Population::next_generation(){
   
+}
+
+int main()
+{
+  Population population()
+
 }
